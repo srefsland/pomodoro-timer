14571d6dfff232fb475e65f8ff18abef6e0e522d
Simen Refsland <simen_refsland@hotmail.com>
2024-03-12 16:25:21 +0100

    remove bold font weight from timer control buttons

--- app/_components/timer/timer-control-button.tsx
+++ app/_components/timer/timer-control-button.tsx
@@ -8,8 +8,7 @@ type TimerControlButtonProps = {
 export default function TimerControlButton(props: TimerControlButtonProps) {
   return (
     <button
-      className="bg-black/85 hover:bg-white/85 hover:text-black text-white transition ease-in-out
-       font-bold h-10 w-16 rounded"
+      className="bg-black/85 hover:bg-white/85 hover:text-black text-white transition ease-in-out h-10 w-16 rounded"
       onClick={props.onClick}
     >
       {props.label}
