b7882ac41ed1c6cce2b786a8c34284ae6dceb330
Simen Refsland <simen_refsland@hotmail.com>
2024-03-14 22:51:30 +0100

    add github link

--- app/_components/settings-bar.tsx
+++ app/_components/settings-bar.tsx
@@ -6,7 +6,7 @@ export default function SettingsBar({
   onSettingsClick: () => void;
 }) {
   return (
-    <div className="absolute top-0 right-0 flex mt-4 mr-4 gap-4">
+    <div className="absolute top-4 right-4 flex gap-4">
       <button
         className="bg-indigo-950/75 p-2 rounded-md hover:bg-indigo-950/60 transition ease-in-out"
         onClick={onSettingsClick}
@@ -14,7 +14,7 @@ export default function SettingsBar({
         Settings <IoMdSettings className="inline" />
       </button>
       <button className="bg-indigo-950/75 p-2 rounded-md hover:bg-indigo-950/60">
-        <IoMdInformationCircleOutline className="inline" />
+        About <IoMdInformationCircleOutline className="inline" />
       </button>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,13 @@
 
 import Image from "next/image";
 import { useState } from "react";
+import { IoLogoGithub } from "react-icons/io5";
 import lofi from "../public/lofi.png";
 import SettingsModal from "./_components/modal/settings-modal";
 import SettingsBar from "./_components/settings-bar";
 import Timer from "./_components/timer/timer";
+
+const REPO_URL = "https://github.com/simenrefsland/pomodoro-timer";
 
 export default function Home() {
   const [isSettingsOpen, setIsSettingsOpen] = useState(false);
@@ -29,6 +32,13 @@ export default function Home() {
         <div className="bg-red-300/25 p-6 rounded-md relative">
           <Timer />
         </div>
+        <a
+          href={REPO_URL}
+          target="_blank"
+          className="fixed bottom-4 right-4 bg-black/85 hover:bg-white/85 hover:text-black text-white p-2 rounded-md transition ease-in-out"
+        >
+          <IoLogoGithub className="inline size-6" />
+        </a>
       </div>
       <SettingsModal
         isOpen={isSettingsOpen}
