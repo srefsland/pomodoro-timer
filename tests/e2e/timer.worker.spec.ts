71c362d6cfd40c45d875ee7f25e5d7dbce9ed91d
Simen Refsland <simen_refsland@hotmail.com>
2025-05-02 14:39:13 +0200

    add web server configuration for playwright tests

--- tests/e2e/timer.worker.spec.ts
+++ tests/e2e/timer.worker.spec.ts
@@ -2,7 +2,8 @@ import { test, expect } from "@playwright/test";
 
 test("Timer Worker", async ({ page }) => {
   await page.goto("http://localhost:3000");
-  await page.waitForTimeout(1000);
+
+  page.waitForLoadState("domcontentloaded");
 
   // Expect to show 25:00 on timer
   const timer = page.getByTestId("timer-clock");
@@ -25,7 +26,6 @@ test("Timer Worker", async ({ page }) => {
   // Click on the reset button
   const resetButton = page.getByTestId("reset");
   await resetButton.click();
-  await page.waitForTimeout(1000);
 
   // Expect to show 25:00 on timer
   await expect(timer).toHaveText("25:00");
