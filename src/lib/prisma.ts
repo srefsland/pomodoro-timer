f105cfaf857658d771d0720bdd0a3f8a41d39cc5
Simen Refsland <simen_refsland@hotmail.com>
2025-05-12 23:40:06 +0200

    add prisma

---
+++ src/lib/prisma.ts
@@ -0,0 +1,12 @@
+import "server-only";
+
+import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+const prisma = new PrismaClient().$extends(withAccelerate());
+
+const globalForPrisma = global as unknown as { prisma: typeof prisma };
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
